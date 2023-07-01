9a9a04acc9212b461e6c5006cf9c60378ee7aa7d
Volodymyr-Savytskyi <132827170+Volodymyr-Savytskyi@users.noreply.github.com>
2023-07-01 15:49:22 +0300

    first check

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -1,28 +1,14 @@
 (() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
-  
-    const toggleMenu = () => {
-      const isMenuOpen =
-        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('is-open');
-  
-      const scrollLockMethod = !isMenuOpen
-        ? 'disableBodyScroll'
-        : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
-    };
-  
-    openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
-  
-    // Close the mobile menu on wider screens if the device orientation changes
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      mobileMenu.classList.remove('is-open');
-      openMenuBtn.setAttribute('aria-expanded', false);
-      bodyScrollLock.enableBodyScroll(document.body);
-    });
-  })();
+	const reft = {
+		openMenuBtn: document.querySelector("[data-menu-open]"),
+		closeMenuBtn: document.querySelector("[data-menu-close]"),
+		menu: document.querySelector("[data-menu]"),
+	};
+
+	reft.openMenuBtn.addEventListener("click", toggleModal);
+	reft.closeMenuBtn.addEventListener("click", toggleModal);
+
+	function toggleModal() {
+		reft.menu.classList.toggle("is-open");
+	}
+})();
